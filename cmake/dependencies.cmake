afb0efd48db84195ee2681c40bc41c0df59cf741
Nikolay Tsoy <nikolay@tsoy.dev>
2025-03-17 13:38:43 +0100

    Use FetchContent for dependency management
    - Also update cmake presets

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,3 +1,18 @@
+include(FetchContent)
+
 find_package(CUDAToolkit REQUIRED)
-find_package(gsl-lite REQUIRED)
-find_package(fmt REQUIRED)
+find_package(OpenMP REQUIRED)
+
+FetchContent_Declare(
+    gsl-lite
+    GIT_REPOSITORY https://github.com/gsl-lite/gsl-lite.git
+    GIT_TAG v0.42.0
+    SYSTEM)
+FetchContent_Declare(
+    fmt
+    GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+    GIT_TAG 11.1.4
+    SYSTEM)
+
+FetchContent_MakeAvailable(gsl-lite)
+FetchContent_MakeAvailable(fmt)
