01665c2d877979b430f0e5fbea3ba4ad41874165
Nikolay Tsoy <nikolay@tsoy.dev>
2025-03-18 15:30:59 +0100

    Remove unnecessary namespace specifier in utils

--- src/utils/include/utils/nip.cuh
+++ src/utils/include/utils/nip.cuh
@@ -12,49 +12,49 @@ auto get_nip(const CSR<T, Location::Device>& a, const CSR<T, Location::Device>& 
     -> int64_t {
     // Initialize cuSPARSE
     cusparseHandle_t handle{};
-    utils::handle_cusparse_error(cusparseCreate(&handle));
+    handle_cusparse_error(cusparseCreate(&handle));
 
     // Create cuSPARSE matrix descriptors
     cusparseSpMatDescr_t desc_a{};
-    utils::handle_cusparse_error(cusparseCreateCsr(&desc_a,
-                                                   a.m,
-                                                   a.n,
-                                                   a.nnz,
-                                                   a.rows_ptr,
-                                                   a.cols,
-                                                   a.values,
-                                                   CUSPARSE_INDEX_32I,
-                                                   CUSPARSE_INDEX_32I,
-                                                   CUSPARSE_INDEX_BASE_ZERO,
-                                                   CUDA_R_64F));
+    handle_cusparse_error(cusparseCreateCsr(&desc_a,
+                                            a.m,
+                                            a.n,
+                                            a.nnz,
+                                            a.rows_ptr,
+                                            a.cols,
+                                            a.values,
+                                            CUSPARSE_INDEX_32I,
+                                            CUSPARSE_INDEX_32I,
+                                            CUSPARSE_INDEX_BASE_ZERO,
+                                            CUDA_R_64F));
     cusparseSpMatDescr_t desc_b{};
-    utils::handle_cusparse_error(cusparseCreateCsr(&desc_b,
-                                                   b.m,
-                                                   b.n,
-                                                   b.nnz,
-                                                   b.rows_ptr,
-                                                   b.cols,
-                                                   b.values,
-                                                   CUSPARSE_INDEX_32I,
-                                                   CUSPARSE_INDEX_32I,
-                                                   CUSPARSE_INDEX_BASE_ZERO,
-                                                   CUDA_R_64F));
+    handle_cusparse_error(cusparseCreateCsr(&desc_b,
+                                            b.m,
+                                            b.n,
+                                            b.nnz,
+                                            b.rows_ptr,
+                                            b.cols,
+                                            b.values,
+                                            CUSPARSE_INDEX_32I,
+                                            CUSPARSE_INDEX_32I,
+                                            CUSPARSE_INDEX_BASE_ZERO,
+                                            CUDA_R_64F));
     cusparseSpMatDescr_t desc_c{};
-    utils::handle_cusparse_error(cusparseCreateCsr(&desc_c,
-                                                   a.m,
-                                                   b.n,
-                                                   0,
-                                                   nullptr,
-                                                   nullptr,
-                                                   nullptr,
-                                                   CUSPARSE_INDEX_32I,
-                                                   CUSPARSE_INDEX_32I,
-                                                   CUSPARSE_INDEX_BASE_ZERO,
-                                                   CUDA_R_64F));
+    handle_cusparse_error(cusparseCreateCsr(&desc_c,
+                                            a.m,
+                                            b.n,
+                                            0,
+                                            nullptr,
+                                            nullptr,
+                                            nullptr,
+                                            CUSPARSE_INDEX_32I,
+                                            CUSPARSE_INDEX_32I,
+                                            CUSPARSE_INDEX_BASE_ZERO,
+                                            CUDA_R_64F));
 
     // Initialize cuSPARSE SpGEMM descriptor
     cusparseSpGEMMDescr_t spgemm_desc{};
-    utils::handle_cusparse_error(cusparseSpGEMM_createDescr(&spgemm_desc));
+    handle_cusparse_error(cusparseSpGEMM_createDescr(&spgemm_desc));
 
     // Set parameters for the algorithm
     const double alpha = 1.0;
@@ -62,48 +62,46 @@ auto get_nip(const CSR<T, Location::Device>& a, const CSR<T, Location::Device>& 
 
     // First stage
     size_t buffer1_size{};
-    utils::handle_cusparse_error(
-        cusparseSpGEMM_workEstimation(handle,
-                                      CUSPARSE_OPERATION_NON_TRANSPOSE,
-                                      CUSPARSE_OPERATION_NON_TRANSPOSE,
-                                      &alpha,
-                                      desc_a,
-                                      desc_b,
-                                      &beta,
-                                      desc_c,
-                                      CUDA_R_64F,
-                                      CUSPARSE_SPGEMM_DEFAULT,
-                                      spgemm_desc,
-                                      &buffer1_size,
-                                      nullptr));
+    handle_cusparse_error(cusparseSpGEMM_workEstimation(handle,
+                                                        CUSPARSE_OPERATION_NON_TRANSPOSE,
+                                                        CUSPARSE_OPERATION_NON_TRANSPOSE,
+                                                        &alpha,
+                                                        desc_a,
+                                                        desc_b,
+                                                        &beta,
+                                                        desc_c,
+                                                        CUDA_R_64F,
+                                                        CUSPARSE_SPGEMM_DEFAULT,
+                                                        spgemm_desc,
+                                                        &buffer1_size,
+                                                        nullptr));
     void* buffer1{};
     handle_cuda_error(cudaMalloc(&buffer1, buffer1_size));
-    utils::handle_cusparse_error(
-        cusparseSpGEMM_workEstimation(handle,
-                                      CUSPARSE_OPERATION_NON_TRANSPOSE,
-                                      CUSPARSE_OPERATION_NON_TRANSPOSE,
-                                      &alpha,
-                                      desc_a,
-                                      desc_b,
-                                      &beta,
-                                      desc_c,
-                                      CUDA_R_64F,
-                                      CUSPARSE_SPGEMM_DEFAULT,
-                                      spgemm_desc,
-                                      &buffer1_size,
-                                      buffer1));
+    handle_cusparse_error(cusparseSpGEMM_workEstimation(handle,
+                                                        CUSPARSE_OPERATION_NON_TRANSPOSE,
+                                                        CUSPARSE_OPERATION_NON_TRANSPOSE,
+                                                        &alpha,
+                                                        desc_a,
+                                                        desc_b,
+                                                        &beta,
+                                                        desc_c,
+                                                        CUDA_R_64F,
+                                                        CUSPARSE_SPGEMM_DEFAULT,
+                                                        spgemm_desc,
+                                                        &buffer1_size,
+                                                        buffer1));
 
     // Extract the number of intermediate products
     int64_t nip{};
-    utils::handle_cusparse_error(cusparseSpGEMM_getNumProducts(spgemm_desc, &nip));
+    handle_cusparse_error(cusparseSpGEMM_getNumProducts(spgemm_desc, &nip));
 
     // Clean up
     handle_cuda_error(cudaFree(buffer1));
-    utils::handle_cusparse_error(cusparseSpGEMM_destroyDescr(spgemm_desc));
-    utils::handle_cusparse_error(cusparseDestroySpMat(desc_c));
-    utils::handle_cusparse_error(cusparseDestroySpMat(desc_b));
-    utils::handle_cusparse_error(cusparseDestroySpMat(desc_a));
-    utils::handle_cusparse_error(cusparseDestroy(handle));
+    handle_cusparse_error(cusparseSpGEMM_destroyDescr(spgemm_desc));
+    handle_cusparse_error(cusparseDestroySpMat(desc_c));
+    handle_cusparse_error(cusparseDestroySpMat(desc_b));
+    handle_cusparse_error(cusparseDestroySpMat(desc_a));
+    handle_cusparse_error(cusparseDestroy(handle));
 
     return nip;
 }
