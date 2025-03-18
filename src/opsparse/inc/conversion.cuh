4b3ff7153c8f35ed3ed5a774d28ff43d86916fb6
Nikolay Tsoy <nikolay@tsoy.dev>
2025-03-18 12:52:36 +0100

    Add CSR conversion functions to opsparse

---
+++ src/opsparse/inc/conversion.cuh
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <utils/utils.cuh>
+#include <CSR.h>
+
+using HostCSR = utils::CSR<double, utils::Location::Host>;
+
+inline auto convertFromUtilsCSR(const HostCSR &from) -> CSR {
+    CSR to;
+    to.M = from.m;
+    to.N = from.n;
+    to.nnz = from.nnz;
+    to.rpt = new mint[to.M + 1];
+    to.col = new mint[to.nnz];
+    to.val = new mdouble[to.nnz];
+    std::copy_n(from.rows_ptr, to.M + 1, to.rpt);
+    std::copy_n(from.cols, to.nnz, to.col);
+    std::copy_n(from.values, to.nnz, to.val);
+    to.d_rpt = nullptr;
+    to.d_col = nullptr;
+    to.d_val = nullptr;
+    return to;
+}
+
+inline auto convertToUtilsCSR(const CSR& from) -> HostCSR {
+    HostCSR to(from.nnz, from.M, from.N);
+    std::copy_n(from.rpt, from.M + 1, to.rows_ptr);
+    std::copy_n(from.col, from.nnz, to.cols);
+    std::copy_n(from.val, from.nnz, to.values);
+    return to;
+}
