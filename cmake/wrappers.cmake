00e400a10254cd5aacbfc1f3987fac6ed6b00254
Nikolay Tsoy <nikolay@tsoy.dev>
2025-03-20 13:46:48 +0100

    Add cmake wrappers for creating targets

---
+++ cmake/wrappers.cmake
@@ -0,0 +1,34 @@
+function(add_executable_wrapped name)
+    add_executable(${name} ${ARGN})
+    _customize_target_wrapped(${name} PRIVATE)
+endfunction()
+
+function(add_library_wrapped name visibility)
+    if(visibility STREQUAL "INTERFACE")
+        add_library(${name} INTERFACE ${ARGN})
+    else()
+        add_library(${name} ${ARGN})
+    endif()
+    _customize_target_wrapped(${name} ${visibility})
+endfunction()
+
+function(_customize_target_wrapped name visibility)
+    target_compile_features(${name} ${visibility} cxx_std_20 cuda_std_20)
+    set_target_properties(
+        ${name}
+        PROPERTIES CXX_EXTENSIONS OFF
+                   CUDA_EXTENSIONS OFF
+                   CUDA_SEPARABLE_COMPILATION ON)
+
+    if(${CMAKE_CUDA_COMPILER_ID} STREQUAL "NVIDIA")
+        target_compile_options(
+            ${name} ${visibility} --extra-device-vectorization --use_fast_math
+            --restrict --expt-relaxed-constexpr)
+    endif()
+
+    target_compile_definitions(${name} ${visibility}
+                                       CUDA_API_PER_THREAD_DEFAULT_STREAM)
+
+    target_link_libraries(${name} ${visibility} gsl-lite)
+    target_link_libraries(${name} ${visibility} fmt::fmt)
+endfunction()
