568e8d2e0e8b963298ee894f29b97c663efb8314
Nikolay Tsoy <nikolay@tsoy.dev>
2025-03-21 18:22:30 +0100

    Add opsparse benchmark executable

---
+++ src/apps/opsparse_benchmark.cu
@@ -0,0 +1,78 @@
+#include <chrono>
+#include <cstdlib>
+#include <thread>
+
+#include <benchmark/benchmark.h>
+#include <fmt/base.h>
+
+#include <opsparse/conversion.cuh>
+#include <opsparse/opsparse.h>
+#include <utils/utils.cuh>
+
+namespace {
+
+constexpr auto SLEEP_TIME = std::chrono::milliseconds(100);
+
+template<std::floating_point T>
+void benchmark_opsparse(benchmark::State& state,
+                        const utils::HostCSR<T>& a,
+                        const utils::HostCSR<T>& b) {
+    // Convert to OpSparse CSR format
+    CSR A = convertFromUtilsCSR(a);
+    CSR B = convertFromUtilsCSR(b);
+    A.H2D();
+    B.H2D();
+
+    for (auto _ : state) {
+        CSR C;
+        Meta meta;
+        Timings timing;
+
+        // Very important observation:
+        // It seems like even after the SpGEMM call returns,
+        // and the device is synchronized, the device is still
+        // busy behind the scenes. So, we need to wait a little
+        // bit and let the device finish operations from the
+        // previous iteration. It significantly improves performance
+        // of each iteration.
+        std::this_thread::sleep_for(SLEEP_TIME);
+
+        const auto start = std::chrono::high_resolution_clock::now();
+        opsparse(A, B, C, meta, timing);
+        utils::handle_cuda_error(cudaDeviceSynchronize());
+        const auto end = std::chrono::high_resolution_clock::now();
+        const auto seconds = std::chrono::duration<double>(end - start).count();
+        state.SetIterationTime(seconds);
+    }
+}
+
+} // namespace
+
+auto main(int argc, char** argv) -> int {
+    using ValueType = double;
+
+    // Load the matrices
+    if (argc < 3) {
+        fmt::println("Usage: {} <input:A> <input:B> [benchmark-options]", argv[0]);
+        return EXIT_FAILURE;
+    }
+    auto h_a = utils::HostCSR<ValueType>::load_from_filename(argv[1]);
+    auto h_b = utils::HostCSR<ValueType>::load_from_filename(argv[2]);
+    if (h_a.n != h_b.m) {
+        fmt::println("Matrix A columns ({}) must match matrix B rows ({})", h_a.n, h_b.m);
+        return EXIT_FAILURE;
+    }
+
+    // Register the benchmark
+    benchmark::RegisterBenchmark("opsparse", benchmark_opsparse<ValueType>, h_a, h_b)
+        ->UseManualTime();
+
+    // Run the benchmark
+    benchmark::Initialize(&argc, argv);
+    benchmark::AddCustomContext("Matrix A", argv[1]);
+    benchmark::AddCustomContext("Matrix B", argv[2]);
+    benchmark::RunSpecifiedBenchmarks();
+    benchmark::Shutdown();
+
+    return EXIT_SUCCESS;
+}
