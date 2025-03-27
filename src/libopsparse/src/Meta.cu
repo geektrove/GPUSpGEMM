028f4c0fb27b2991bb5ac192b80bafe71d43f47f
Nikolay Tsoy <nikolay@tsoy.dev>
2025-03-27 12:24:46 +0100

    Update Meta.cu

--- src/libopsparse/src/Meta.cu
+++ src/libopsparse/src/Meta.cu
@@ -49,9 +49,9 @@ void Meta::allocate(CSR& C) {
 }
 
 void Meta::release() {
-    #ifndef NVTX_DISABLE
+#ifndef NVTX_DISABLE
     nvtx3::scoped_range r{"cleanup"};
-    #endif
+#endif
     cudaFree(d_combined_mem);
     d_combined_mem = nullptr;
     if (stream != nullptr) {
