39dd1b4dd7e85b99d1239f6dc43c34c861d6b1cd
Nikolay Tsoy <nikolay@tsoy.dev>
2025-03-29 18:28:02 +0100

    Check nnz in shared memory only when updating it

--- src/libproposal/include/proposal/sym.cuh
+++ src/libproposal/include/proposal/sym.cuh
@@ -194,23 +194,26 @@ __global__ void k_sym_smem_max(
     const auto threshold = table_size * SYM_RANGE_RATIO;
     block.sync();
 
-    for (auto i = a_rpt[row] + i_offset; i < a_rpt[row + 1]; i += i_step) {
-        const auto colrow = a_col[i];
-        for (auto k = b_rpt[colrow] + k_offset; k < b_rpt[colrow + 1]; k += k_step) {
-            const auto key = b_col[k];
-            auto hash = (key * HASH_SCALE) % table_size;
-            while (*s_nnz <= threshold) {
-                const auto old = atomicCAS_block(s_table + hash, -1, key);
-                if (old == -1) {
-                    atomicAdd_block(s_nnz, 1);
-                    break;
+    cuda::std::invoke([&] {
+        for (auto i = a_rpt[row] + i_offset; i < a_rpt[row + 1]; i += i_step) {
+            const auto colrow = a_col[i];
+            for (auto k = b_rpt[colrow] + k_offset; k < b_rpt[colrow + 1]; k += k_step) {
+                const auto key = b_col[k];
+                auto hash = (key * HASH_SCALE) % table_size;
+                while (true) {
+                    const auto old = atomicCAS_block(s_table + hash, -1, key);
+                    if (old == -1) {
+                        if (atomicAdd_block(s_nnz, 1) >= threshold)
+                            return;
+                        break;
+                    }
+                    if (old == key)
+                        break;
+                    hash = hash + 1 < table_size ? hash + 1 : 0;
                 }
-                if (old == key)
-                    break;
-                hash = hash + 1 < table_size ? hash + 1 : 0;
             }
         }
-    }
+    });
     block.sync();
 
     cg::invoke_one(block, [&] {
