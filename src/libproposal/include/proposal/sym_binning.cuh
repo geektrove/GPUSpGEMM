a3e8157971dc16659f177f76e3da3a9d3141e300
Nikolay Tsoy <nikolay@tsoy.dev>
2025-03-29 22:18:23 +0100

    Extract small binning code into function

--- src/libproposal/include/proposal/sym_binning.cuh
+++ src/libproposal/include/proposal/sym_binning.cuh
@@ -32,6 +32,32 @@ __global__ void k_sym_binning2(
     __grid_constant__ std::int32_t* const __restrict__ bin_sizes,
     __grid_constant__ std::int32_t* const __restrict__ bins);
 
+inline void small_binning(const std::int32_t m, Meta& meta) {
+    auto op = [d_bins = meta.d_bins] __device__(int i) {
+        d_bins[i] = static_cast<std::int32_t>(i);
+    };
+
+    if constexpr (utils::IS_DEBUG) {
+        size_t cub_requested{};
+        cub::DeviceFor::Bulk(nullptr, cub_requested, m, op);
+        assert(cub_requested <= meta.cub_storage_size);
+    }
+
+    // Perform iota operation to fill the smallest bin with row indices
+    utils::handle_cuda_error(
+        cub::DeviceFor::Bulk(meta.d_cub_storage, meta.cub_storage_size, m, op));
+
+    // Set bin sizes and offsets
+    meta.h_bin_sizes[0] = m;
+    for (int i = 1; i < meta.n_bins; i++)
+        meta.h_bin_sizes[i] = 0;
+    meta.h_bin_offsets[0] = 0;
+    for (int i = 1; i < meta.n_bins; i++)
+        meta.h_bin_offsets[i] = m;
+
+    utils::stream_sync();
+}
+
 template<std::floating_point T>
 void sym_binning(utils::DeviceCSR<T>& C, Meta& meta, const Device& device) {
     NVTX3_FUNC_RANGE();
@@ -39,29 +65,7 @@ void sym_binning(utils::DeviceCSR<T>& C, Meta& meta, const Device& device) {
     if (*meta.h_max_row_nnz <= meta.h_sym_bin_ranges[0]) {
         // If all rows fall into the smallest bin, we can skip the binning process
         // and directly assign the row indices to the smallest bin
-        auto op = [d_bins = meta.d_bins] __device__(int i) {
-            d_bins[i] = static_cast<std::int32_t>(i);
-        };
-
-        if constexpr (utils::IS_DEBUG) {
-            size_t cub_requested{};
-            cub::DeviceFor::Bulk(nullptr, cub_requested, C.m, op);
-            assert(cub_requested <= meta.cub_storage_size);
-        }
-
-        // Perform iota operation to fill the smallest bin with row indices
-        utils::handle_cuda_error(
-            cub::DeviceFor::Bulk(meta.d_cub_storage, meta.cub_storage_size, C.m, op));
-
-        // Set bin sizes and offsets
-        meta.h_bin_sizes[0] = C.m;
-        for (int i = 1; i < meta.n_bins; i++)
-            meta.h_bin_sizes[i] = 0;
-        meta.h_bin_offsets[0] = 0;
-        for (int i = 1; i < meta.n_bins; i++)
-            meta.h_bin_offsets[i] = C.m;
-
-        utils::stream_sync();
+        small_binning(C.m, meta);
         return;
     }
 
