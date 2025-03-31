1f4bd95e544ab41049c4a442e7e8b7530b36b109
Nikolay Tsoy <nikolay@tsoy.dev>
2025-03-31 16:48:58 +0200

    Update binning.cuh

--- src/libproposal/include/proposal/binning.cuh
+++ src/libproposal/include/proposal/binning.cuh
@@ -7,14 +7,14 @@
 __global__ void k_binning1(
     const __grid_constant__ std::int32_t* const __restrict__ ranges,
     const __grid_constant__ std::int32_t n_bins,
-    const __grid_constant__ std::int32_t* const __restrict__ nips,
+    const __grid_constant__ std::int32_t* const __restrict__ values,
     const __grid_constant__ std::int32_t m,
     __grid_constant__ std::int32_t* const __restrict__ bin_sizes);
 
 __global__ void k_binning2(
     const __grid_constant__ std::int32_t* const __restrict__ ranges,
     const __grid_constant__ std::int32_t n_bins,
-    const __grid_constant__ std::int32_t* const __restrict__ nips,
+    const __grid_constant__ std::int32_t* const __restrict__ values,
     const __grid_constant__ std::int32_t m,
     const __grid_constant__ std::int32_t* const __restrict__ bin_offsets,
     __grid_constant__ std::int32_t* const __restrict__ bin_sizes,
