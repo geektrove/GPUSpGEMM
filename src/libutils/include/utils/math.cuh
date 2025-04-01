a27ba467dc3c438561382fd801a51533c3881bc7
Nikolay Tsoy <nikolay@tsoy.dev>
2025-04-01 15:06:51 +0200

    Fix ordering of functions in math

--- src/libutils/include/utils/math.cuh
+++ src/libutils/include/utils/math.cuh
@@ -11,41 +11,6 @@
 namespace utils {
 
 template<std::integral T>
-constexpr auto ispow2(T a) -> bool {
-    assert(a > 0);
-    if (a <= 0)
-        __builtin_unreachable();
-    const auto unsigned_a = gsl::narrow_cast<std::make_unsigned_t<T>>(a);
-    return cuda::std::has_single_bit(unsigned_a);
-}
-
-constexpr auto modpow2(std::integral auto a, std::integral auto b) {
-    assert(b > 0);
-    assert(ispow2(b));
-    if (b <= 0)
-        __builtin_unreachable();
-    return a & (b - 1);
-}
-
-constexpr auto divpow2(std::integral auto a, std::integral auto b) {
-    assert(b > 0);
-    assert(ispow2(b));
-    if (b <= 0)
-        __builtin_unreachable();
-    return a >> ilog2(b);
-}
-
-template<std::integral T>
-constexpr auto ilog2(T a) {
-    assert(a > 0);
-    if (a <= 0)
-        __builtin_unreachable();
-    const auto unsigned_a = gsl::narrow_cast<std::make_unsigned_t<T>>(a);
-    const auto result = cuda::std::bit_width(unsigned_a) - 1;
-    return gsl::narrow_cast<T>(result);
-}
-
-template<std::integral T>
 constexpr auto bitceil(T a) {
     assert(a > 0);
     if (a <= 0)
@@ -63,6 +28,41 @@ constexpr auto bitfloor(T a) {
     const auto unsigned_a = gsl::narrow_cast<std::make_unsigned_t<T>>(a);
     const auto result = cuda::std::bit_floor(unsigned_a);
     return gsl::narrow_cast<T>(result);
+}
+
+template<std::integral T>
+constexpr auto ispow2(T a) -> bool {
+    assert(a > 0);
+    if (a <= 0)
+        __builtin_unreachable();
+    const auto unsigned_a = gsl::narrow_cast<std::make_unsigned_t<T>>(a);
+    return cuda::std::has_single_bit(unsigned_a);
+}
+
+template<std::integral T>
+constexpr auto ilog2(T a) {
+    assert(a > 0);
+    if (a <= 0)
+        __builtin_unreachable();
+    const auto unsigned_a = gsl::narrow_cast<std::make_unsigned_t<T>>(a);
+    const auto result = cuda::std::bit_width(unsigned_a) - 1;
+    return gsl::narrow_cast<T>(result);
+}
+
+constexpr auto modpow2(std::integral auto a, std::integral auto b) {
+    assert(b > 0);
+    assert(ispow2(b));
+    if (b <= 0)
+        __builtin_unreachable();
+    return a & (b - 1);
+}
+
+constexpr auto divpow2(std::integral auto a, std::integral auto b) {
+    assert(b > 0);
+    assert(ispow2(b));
+    if (b <= 0)
+        __builtin_unreachable();
+    return a >> ilog2(b);
 }
 
 } // namespace utils
