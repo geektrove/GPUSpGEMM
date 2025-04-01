a10380240a6eda79d1520b6d79cf1c7a5a1c936c
Nikolay Tsoy <nikolay@tsoy.dev>
2025-04-02 00:11:34 +0200

    Update parameters.cuh

--- src/libproposal/include/proposal/parameters.cuh
+++ src/libproposal/include/proposal/parameters.cuh
@@ -6,8 +6,8 @@
 
 inline constexpr std::int32_t HASH_EMPTY = -1;
 inline constexpr std::int32_t HASH_SCALE = 107;
-inline constexpr std::int32_t SYM_PWARP_SIZE = 4;
 inline constexpr double SYM_RANGE_RATIO = 1 / 1.2;
+inline constexpr std::int32_t SYM_PWARP_SIZE = 4;
 inline constexpr std::int32_t NUM_PWARP_SIZE = 8;
 inline constexpr std::int32_t N_CUDA_EVENTS = 1;
 
