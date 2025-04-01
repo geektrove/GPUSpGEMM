c48cdfcab867eb64d7f4a26a3e31afb44f2bb557
Nikolay Tsoy <nikolay@tsoy.dev>
2025-04-01 15:04:40 +0200

    Update meta.cuh

--- src/libproposal/include/proposal/meta.cuh
+++ src/libproposal/include/proposal/meta.cuh
@@ -4,10 +4,11 @@
 #include <cstdint>
 
 inline constexpr std::int32_t WARP_SIZE = 32;
-inline constexpr std::int32_t SYM_PWARP_SIZE = 4;
-inline constexpr std::int32_t NUM_PWARP_SIZE = 8;
 
+inline constexpr std::int32_t SYM_PWARP_SIZE = 4;
 inline constexpr double SYM_RANGE_RATIO = 1 / 1.2;
+
+inline constexpr std::int32_t NUM_PWARP_SIZE = 8;
 
 struct Meta {
     std::array<cudaEvent_t, 1> events{}; // size 1
