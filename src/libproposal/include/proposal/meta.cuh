cf52349f82dc1acdc3e19acaccd669ab8d69ed56
Nikolay Tsoy <nikolay@tsoy.dev>
2025-04-01 23:27:43 +0200

    Update meta.cuh

--- src/libproposal/include/proposal/meta.cuh
+++ src/libproposal/include/proposal/meta.cuh
@@ -3,28 +3,26 @@
 #include <array>
 #include <cstdint>
 
-struct Meta {
-    std::array<cudaEvent_t, 1> events{}; // size 1
-    cudaStream_t* streams{};             // size n_bins
+#include <proposal/parameters.cuh>
 
-    std::int32_t n_bins{};
+template<typename Params>
+struct Meta {
+    std::array<cudaEvent_t, N_CUDA_EVENTS> events{};        // size 1
+    std::array<cudaStream_t, Params::MAX_N_BINS> streams{}; // size n_bins
+
+    // std::int32_t n_bins{};
     std::size_t cub_storage_size{};
     std::size_t mem_pool_size{};
 
-    // Host memory (combined)
-    void* h_ptr{};
-    std::int32_t* block_sizes{};   // size n_bins
-    std::int32_t* table_sizes{};   // size n_bins
-    std::int32_t* h_bin_ranges{};  // size n_bins
-    std::int32_t* h_bin_sizes{};   // size n_bins
-    std::int32_t* h_bin_offsets{}; // size n_bins
-    std::int32_t* h_max_row_nnz{}; // size 1
-    std::int32_t* h_total_nnz{};   // size 1
+    // Host memory
+    std::int32_t h_bin_sizes[Params::MAX_N_BINS]{};
+    std::int32_t h_bin_offsets[Params::MAX_N_BINS]{};
+    std::int32_t h_max_row_nnz{};
+    std::int32_t h_total_nnz{};
 
     // Device memory (combined)
     void* d_ptr{};
     std::int32_t* d_bins{};        // size M
-    std::int32_t* d_bin_ranges{};  // size n_bins
     std::int32_t* d_bin_sizes{};   // size n_bins
     std::int32_t* d_bin_offsets{}; // size n_bins
     std::int32_t* d_max_row_nnz{}; // size 1
