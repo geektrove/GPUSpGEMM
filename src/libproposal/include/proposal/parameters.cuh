39b40aaa020c1a4f788cf0a7657d47cd487c3a99
Nikolay Tsoy <nikolay@tsoy.dev>
2025-04-01 23:27:26 +0200

    Create parameters.cuh

---
+++ src/libproposal/include/proposal/parameters.cuh
@@ -0,0 +1,86 @@
+#pragma once
+
+#include <algorithm>
+#include <cstdint>
+#include <cuda/std/array>
+
+inline constexpr std::int32_t HASH_EMPTY = -1;
+inline constexpr std::int32_t HASH_SCALE = 107;
+inline constexpr std::int32_t SYM_PWARP_SIZE = 4;
+inline constexpr double SYM_RANGE_RATIO = 1 / 1.2;
+inline constexpr std::int32_t NUM_PWARP_SIZE = 8;
+inline constexpr std::int32_t N_CUDA_EVENTS = 1;
+
+inline constexpr std::int32_t WARP_SIZE = 32;
+
+inline constexpr std::int32_t CC86 = 86;
+
+template<std::int32_t ComputeCapability>
+struct Parameters;
+
+template<>
+struct Parameters<CC86> {
+    // static constexpr std::int32_t CC = CC86;
+    static constexpr std::int32_t OPTIMAL_BLOCK_SIZE = 512;
+
+    static constexpr std::int32_t SYM1_N_BINS = 6;
+    using SYM1_CUDA_ARRAY = cuda::std::array<std::int32_t, SYM1_N_BINS>;
+    static constexpr SYM1_CUDA_ARRAY SYM1_BLOCK_SIZES = {512, 128, 256, 512, 1024, 1024};
+    static constexpr SYM1_CUDA_ARRAY SYM1_TABLE_SIZES =
+        {4096, 1024, 2048, 8192, 16384, 25343};
+    static constexpr SYM1_CUDA_ARRAY SYM1_RANGES =
+        {26, 853, 1706, 6826, 13653, INT32_MAX};
+
+    static constexpr std::int32_t SYM2_N_BINS = 7;
+    using SYM2_CUDA_ARRAY = cuda::std::array<std::int32_t, SYM2_N_BINS>;
+    static constexpr SYM2_CUDA_ARRAY SYM2_BLOCK_SIZES =
+        {512, 128, 256, 512, 1024, 1024, 1024};
+    static constexpr SYM2_CUDA_ARRAY SYM2_TABLE_SIZES =
+        {4096, 1024, 2048, 8192, 16384, 25343, 14859};
+    static constexpr SYM2_CUDA_ARRAY SYM2_RANGES =
+        {26, 853, 1706, 6826, 13653, 21119, INT32_MAX};
+
+    static constexpr std::int32_t NUM_N_BINS = 6;
+    using NUM_CUDA_ARRAY = cuda::std::array<std::int32_t, NUM_N_BINS>;
+    static constexpr NUM_CUDA_ARRAY NUM_BLOCK_SIZES = {512, 128, 256, 512, 1024, 1024};
+    static constexpr NUM_CUDA_ARRAY NUM_TABLE_SIZES =
+        {2758, 624, 1336, 2758, 8448, 12383};
+    static constexpr NUM_CUDA_ARRAY NUM_RANGES = {43, 624, 1336, 2758, 8448, 12383};
+
+    static constexpr std::int32_t MAX_N_BINS = std::max(
+        {SYM1_N_BINS, SYM2_N_BINS, NUM_N_BINS});
+};
+
+enum class BinningType : std::int8_t {
+    SYM1,
+    SYM2,
+    NUM
+};
+
+template<typename Params, BinningType BinType>
+__host__ consteval auto get_ranges() {
+    if constexpr (BinType == BinningType::SYM1) {
+        return Params::SYM1_RANGES;
+    } else if constexpr (BinType == BinningType::SYM2) {
+        return Params::SYM2_RANGES;
+    } else if constexpr (BinType == BinningType::NUM) {
+        return Params::NUM_RANGES;
+    }
+}
+
+template<BinningType BinType>
+__device__ consteval auto get_ranges() {
+#ifdef __CUDA_ARCH__
+#if __CUDA_ARCH__ == 860
+    if constexpr (BinType == BinningType::SYM1) {
+        return Parameters<CC86>::SYM1_RANGES;
+    } else if constexpr (BinType == BinningType::SYM2) {
+        return Parameters<CC86>::SYM2_RANGES;
+    } else if constexpr (BinType == BinningType::NUM) {
+        return Parameters<CC86>::NUM_RANGES;
+    }
+#else
+#error "Unsupported compute capability"
+#endif
+#endif
+}
