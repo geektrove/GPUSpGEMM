bc2740c464d260419632f69e5d35f3b6e84432f7
Nikolay Tsoy <nikolay@tsoy.dev>
2025-04-02 13:12:24 +0200

    Update parameters.cuh

--- src/libproposal/include/proposal/parameters.cuh
+++ src/libproposal/include/proposal/parameters.cuh
@@ -20,22 +20,35 @@ struct Parameters;
 
 template<>
 struct Parameters<CC86> {
-    // static constexpr std::int32_t CC = CC86;
+    // Optimal block size for 100% occupancy
     static constexpr std::int32_t OPTIMAL_BLOCK_SIZE = 512;
 
+    //
+    // Symbolic 1
+    //
+
     static constexpr std::int32_t SYM1_N_BINS = 7;
+
     static constexpr std::int32_t SYM1_PWARP_BIN = 0;
+    static constexpr std::int32_t SYM1_PWARP_SIZE = 4;
+    static constexpr std::int32_t SYM1_SMEM_BIN_BEGIN = 4;
     static constexpr std::int32_t SYM1_MAX_SMEM_BIN = 5;
     static constexpr std::int32_t SYM1_GLOBAL_MEM_BIN = 6;
+
     using SYM1_CUDA_ARRAY = cuda::std::array<std::int32_t, SYM1_N_BINS>;
     static constexpr SYM1_CUDA_ARRAY SYM1_BLOCK_SIZES =
         {512, 128, 256, 512, 1024, 1024, 1024};
     static constexpr SYM1_CUDA_ARRAY SYM1_TABLE_SIZES =
-        {4096, 1024, 2048, 8192, 16384, 25343, INT32_MAX};
+        {8192, 1024, 2048, 8192, 16384, 25343, INT32_MAX};
     static constexpr SYM1_CUDA_ARRAY SYM1_RANGES =
-        {26, 853, 1706, 6826, 13653, INT32_MAX, INT32_MAX};
+        {53, 853, 1706, 6826, 13653, INT32_MAX, INT32_MAX};
+
+    //
+    // Symbolic 2
+    //
 
     static constexpr std::int32_t SYM2_N_BINS = 7;
+
     using SYM2_CUDA_ARRAY = cuda::std::array<std::int32_t, SYM2_N_BINS>;
     static constexpr SYM2_CUDA_ARRAY SYM2_BLOCK_SIZES =
         {512, 128, 256, 512, 1024, 1024, 1024};
@@ -44,12 +57,21 @@ struct Parameters<CC86> {
     static constexpr SYM2_CUDA_ARRAY SYM2_RANGES =
         {26, 853, 1706, 6826, 13653, 21119, INT32_MAX};
 
+    //
+    // Numeric
+    //
+
     static constexpr std::int32_t NUM_N_BINS = 6;
+
     using NUM_CUDA_ARRAY = cuda::std::array<std::int32_t, NUM_N_BINS>;
     static constexpr NUM_CUDA_ARRAY NUM_BLOCK_SIZES = {512, 128, 256, 512, 1024, 1024};
     static constexpr NUM_CUDA_ARRAY NUM_TABLE_SIZES =
         {2758, 624, 1336, 2758, 8448, INT32_MAX};
     static constexpr NUM_CUDA_ARRAY NUM_RANGES = {43, 624, 1336, 2758, 8448, INT32_MAX};
+
+    //
+    // General
+    //
 
     static constexpr std::int32_t MAX_N_BINS = std::max(
         {SYM1_N_BINS, SYM2_N_BINS, NUM_N_BINS});
