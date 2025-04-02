58ea1c5cdab597085e95c8a09b6a79482fa0b67c
Nikolay Tsoy <nikolay@tsoy.dev>
2025-04-02 15:38:57 +0200

    Add constexpr_for to utils

--- src/libproposal/include/proposal/utils.cuh
+++ src/libproposal/include/proposal/utils.cuh
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cstdint>
+#include <type_traits>
+
 #include <proposal/device.cuh>
 
 inline auto get_smem_size(const Device& device, const std::int32_t n_blocks)
@@ -9,4 +12,14 @@ inline auto get_smem_size(const Device& device, const std::int32_t n_blocks)
     smem /= n_blocks;                                  // SMEM per block
     smem = std::min(smem, device.max_smem_per_block);  // Max SMEM per block (opt in)
     return smem;
+}
+
+template<std::int32_t begin, std::int32_t end, std::int32_t step, typename F>
+inline void constexpr_for(F&& f) {
+    static_assert(step == 1 || step == -1);
+    static_assert((begin <= end && step > 0) || (begin >= end && step < 0));
+    if constexpr (begin != end) {
+        f(std::integral_constant<std::int32_t, begin>{});
+        constexpr_for<begin + step, end, step>(std::forward<F>(f));
+    }
 }
