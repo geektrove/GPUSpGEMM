5915d4bc974604b3da2d46d9974efbc8abe60a5f
Nikolay Tsoy <nikolay@tsoy.dev>
2025-04-03 18:27:59 +0200

    Update utils.cuh

--- src/libutils/include/utils/utils.cuh
+++ src/libutils/include/utils/utils.cuh
@@ -3,6 +3,7 @@
 #include <utils/csr.cuh>
 #include <utils/definitions.cuh>
 #include <utils/errors.cuh>
+#include <utils/location.cuh>
 #include <utils/math.cuh>
 #include <utils/nip.cuh>
 #include <utils/runtime.cuh>
