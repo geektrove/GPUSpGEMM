2c4cea8f0fb0338238213a70d06e8f3a6f5f9b06
Nikolay Tsoy <nikolay@tsoy.dev>
2025-04-04 12:49:14 +0200

    Implement CLI interface for proposal with validation, saving and benchmarking

--- src/apps/proposal.cu
+++ src/apps/proposal.cu
@@ -1,52 +1,142 @@
 #include <cstdlib>
 #include <thread>
 
-#include <fmt/base.h>
+#include <CLI/CLI.hpp>
+#include <fmt/chrono.h>
+#include <fmt/core.h>
 #include <nvtx3/nvtx3.hpp>
 #include <spdlog/cfg/env.h>
 
+#include <cusparse/cusparse.cuh>
 #include <proposal/proposal.cuh>
 #include <utils/utils.cuh>
 
-constexpr int WARMUP_ITERATIONS = 10;
-constexpr auto SLEEP_TIME = std::chrono::milliseconds(100);
+constexpr auto DEFAULT_PAUSE_MS = 100;
 
 auto main(int argc, char** argv) -> int {
     using ValueType = double;
+    using Clock = std::chrono::steady_clock;
 
-    // Initialize logging
-    spdlog::cfg::load_env_levels();
+    // Parse command line arguments
+    CLI::App app{"Proposal"};
+    app.require_subcommand(1, 1);
 
-    // Initialize NVTX
-    nvtxInitialize(nullptr);
+    app.add_option("inputA")
+        ->description("Path to input matrix A")
+        ->required()
+        ->check(CLI::ExistingFile);
+    app.add_option("inputB")
+        ->description("Path to input matrix B")
+        ->required()
+        ->check(CLI::ExistingFile);
+    app.add_option("--loglevel")
+        ->description("Set the logging level")
+        ->check(
+            CLI::IsMember({"trace", "debug", "info", "warn", "error", "critical", "off"}))
+        ->default_str("info");
 
-    // Load the matrices
-    if (argc != 4) {
-        fmt::println("Usage: {} <input:A> <input:B> <output>", argv[0]);
-        return EXIT_FAILURE;
-    }
-    const auto h_a = utils::HostCSR<ValueType>::load_from_filename(argv[1]);
-    const auto h_b = utils::HostCSR<ValueType>::load_from_filename(argv[2]);
+    app.add_subcommand("validate")
+        ->description("Validate the result matrix using cuSPARSE");
+
+    auto* save = app.add_subcommand("save")->description(
+        "Save the result matrix to a file");
+    save->add_option("output")->description("Path to output matrix")->required();
+
+    auto* benchmark =
+        app.add_subcommand("benchmark")->description("Benchmark the algorithm");
+    benchmark->add_option("--runs")
+        ->description("Number of runs to perform")
+        ->default_val(1)
+        ->check(CLI::NonNegativeNumber);
+    benchmark->add_option("--warmups")
+        ->description("Number of warmup iterations before the actual runs")
+        ->default_val(0)
+        ->check(CLI::NonNegativeNumber);
+    benchmark->add_option("--pause")
+        ->description("Pause between iterations in milliseconds")
+        ->default_val(DEFAULT_PAUSE_MS)
+        ->check(CLI::NonNegativeNumber);
+
+    CLI11_PARSE(app, argc, argv);
+
+    // Set logging level
+    const auto& loglevel = app["--loglevel"]->as<std::string>();
+    spdlog::set_level(spdlog::level::from_str(loglevel));
+
+    // Load matrices
+    const auto& inputA = app["inputA"]->as<std::string>();
+    const auto& inputB = app["inputB"]->as<std::string>();
+    const auto h_a = utils::HostCSR<ValueType>::load_from_filename(inputA);
+    const auto h_b = utils::HostCSR<ValueType>::load_from_filename(inputB);
     if (h_a.n != h_b.m) {
-        fmt::println("Matrix A columns ({}) must match matrix B rows ({})", h_a.n, h_b.m);
+        SPDLOG_ERROR("Matrix A columns ({}) must match matrix B rows ({})", h_a.n, h_b.m);
         return EXIT_FAILURE;
     }
     const auto d_a = h_a.to<utils::Location::Device>();
     const auto d_b = h_b.to<utils::Location::Device>();
 
-    // Warmup
-    for (int i = 0; i < WARMUP_ITERATIONS; i++) {
-        auto c = proposal::proposal(d_a, d_b);
-        utils::device_sync();
-        std::this_thread::sleep_for(SLEEP_TIME);
+    // Validate
+    if (app.got_subcommand("validate")) {
+        const auto h_c = std::invoke(
+            [&] { return proposal::proposal(d_a, d_b).to<utils::Location::Host>(); });
+        const auto h_c_cusparse = std::invoke(
+            [&] { return cusparse(d_a, d_b).to<utils::Location::Host>(); });
+        if (h_c == h_c_cusparse) {
+            fmt::println("Validation succeeded");
+        } else {
+            fmt::println("Validation failed");
+            fmt::println("Matrix A: {} x {}", h_a.m, h_a.n);
+            fmt::println("Matrix B: {} x {}", h_b.m, h_b.n);
+            fmt::println("Matrix C: {} x {}", h_c.m, h_c.n);
+            fmt::println("Matrix C (cuSPARSE): {} x {}", h_c_cusparse.m, h_c_cusparse.n);
+            fmt::println("Non-zero elements in C: {}", h_c.nnz);
+            fmt::println("Non-zero elements in C (cuSPARSE): {}", h_c_cusparse.nnz);
+        }
     }
 
-    // Execute
-    const auto d_c = proposal::proposal(d_a, d_b);
+    // Save
+    if (app.got_subcommand("save")) {
+        const auto& output = save->get_option("output")->as<std::string>();
+        fmt::println("Saving result to {}", output);
+        const auto h_c = std::invoke(
+            [&] { return proposal::proposal(d_a, d_b).to<utils::Location::Host>(); });
+        h_c.save_to_filename(output);
+    }
 
-    // Save the result
-    auto h_c = d_c.to<utils::Location::Host>();
-    h_c.save_to_filename(argv[3]);
+    // Benchmark
+    if (app.got_subcommand("benchmark")) {
+        // Initialize NVTX
+#ifndef NVTX_DISABLE
+        nvtxInitialize(nullptr);
+#endif
+
+        const auto& runs = benchmark->get_option("--runs")->as<int>();
+        const auto& warmups = benchmark->get_option("--warmups")->as<int>();
+        const auto& pause = benchmark->get_option("--pause")->as<int>();
+
+        // Warmup
+        for (int i = 0; i < warmups; i++) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(pause));
+            auto c = proposal::proposal(d_a, d_b);
+            utils::device_sync();
+        }
+
+        // Execute
+        std::vector<Clock::duration> times(runs);
+        for (int i = 0; i < runs; i++) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(pause));
+            const auto start = Clock::now();
+            auto c = proposal::proposal(d_a, d_b);
+            utils::device_sync();
+            const auto end = Clock::now();
+            times[i] = end - start;
+        }
+
+        // Print results
+        fmt::println("Timestamp: {}", std::chrono::system_clock::now());
+        for (int i = 0; i < runs; i++)
+            fmt::println("Run {:2d}: {}", i + 1, times[i]);
+    }
 
     return EXIT_SUCCESS;
 }
