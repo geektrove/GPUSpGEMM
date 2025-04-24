1257542ae62654c2554c56e8bf9e43377cfbe58d
Nikolay Tsoy <nikolay@tsoy.dev>
2025-04-24 18:30:36 +0200

    Add benchmarking scripts for fit cluster

---
+++ scripts/fit-cluster/run-cusparse2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu2
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --output=logs/%x.%j.out
+#SBATCH --error=logs/%x.%j.err
+
+inv validate --preset=release --target=cusparse2
+inv benchmark --target=cusparse2
